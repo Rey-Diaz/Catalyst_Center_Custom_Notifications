7f8f9562b4b92d7b29aeb339d81a09bed2c3ce8b
Rey-Diaz <Reinoldiaz@gmail.com>
2024-01-14 18:26:37 -0500

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Layout from './layout/layout';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/pages/home/home'; // Adjust the path as necessary
+import AppPage from './components/pages/app/app'; // Adjust the path as necessary
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/app" element={<AppPage />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'Frontend/src/index.html')); // Correct path to index.html
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
