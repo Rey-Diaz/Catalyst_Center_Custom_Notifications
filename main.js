6e76644f23ee8196ab4e6e5708bcfd392bba0f4c
Rey-Diaz <Reinoldiaz@gmail.com>
2024-01-14 18:53:19 -0500

    up

--- main.js
+++ main.js
@@ -1,30 +1,55 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const { spawn } = require('child_process');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
+let backendProcess;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
 
-  mainWindow.loadFile(path.join(__dirname, 'Frontend/src/index.html')); // Correct path to index.html
+    mainWindow.loadURL(
+        isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
+    );
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 }
 
-app.whenReady().then(createWindow);
+function startBackend() {
+    backendProcess = spawn('python', ['Catalyst_Center_Custom_Notifications\Frontend\Backend\server.py']);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    backendProcess.stdout.on('data', (data) => {
+        console.log(`Backend: ${data}`);
+    });
+
+    backendProcess.stderr.on('data', (data) => {
+        console.error(`Backend Error: ${data}`);
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    startBackend();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
 });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('will-quit', () => {
+    backendProcess.kill();
 });
